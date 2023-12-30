2a219c92c53b39ddfad1ce30392230024647f98a
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2023-12-30 16:58:28 +0700

    add dashboard content picker example

---
+++ src/components/DashboardApp.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ContentPicker from './ContentPicker.vue';
+import FloatingBar from './FloatingBar.vue';
+
+withDefaults(
+  defineProps<{
+    xContentPicker?: boolean;
+  }>(),
+  {
+    xContentPicker: false,
+  }
+);
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-slate-100">
+    <FloatingBar />
+    <ContentPicker v-if="xContentPicker" />
+  </div>
+</template>
