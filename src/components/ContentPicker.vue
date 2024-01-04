f19b9cf0228d32fe519fd414067f654c730f03e1
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2024-01-04 10:54:25 +0800

    adjust content picker styles

--- src/components/ContentPicker.vue
+++ src/components/ContentPicker.vue
@@ -1,12 +1,66 @@
 <template>
   <div
-    class="absolute inset-0 flex justify-center items-center bg-[rgba(0,0,0,0.4)]"
+    :class="[
+      'absolute',
+      'inset-0',
+
+      'flex',
+      'justify-center',
+      'items-center',
+
+      'pt-40',
+      '@tablet-portrait/surface-container:pt-0',
+
+      'bg-[rgba(0,0,0,0.4)]',
+    ]"
   >
     <div
-      class="flex flex-col justify-center items-center gap-3 @tablet-portrait/surface-container:gap-4 shadow-md shadow-gray-500 rounded-3xl w-[600px] h-[500px] p-4 bg-slate-100"
+      :class="[
+        'flex',
+        'flex-col',
+        'justify-center',
+        'items-center',
+        'gap-3',
+        '@tablet-portrait/surface-container:gap-4',
+
+        'shadow-md',
+        'shadow-gray-500',
+        'rounded-t-3xl',
+        'rounded-ee-2xl',
+        '@tablet-portrait/surface-container:rounded-3xl',
+
+        'w-full',
+        '@tablet-portrait/surface-container:w-[600px]',
+        'h-full',
+        '@tablet-portrait/surface-container:h-[500px]',
+        'p-4',
+
+        'bg-slate-100',
+      ]"
     >
-      <div class="flex flex-col @tablet-portrait/surface-container:flex-row gap-[1px] @tablet-portrait/surface-container:gap-4" v-for="r in 3">
-        <div v-for="c in 3" :key="c" class="bg-amber-500 rounded-lg w-40 h-12"></div>
+      <div
+        :class="[
+          'flex',
+          'flex-col',
+          '@tablet-portrait/surface-container:flex-row',
+          'justify-center',
+          'gap-1',
+          '@tablet-portrait/surface-container:gap-4',
+          'w-full',
+        ]"
+        v-for="r in 3"
+      >
+        <div
+          v-for="c in 3"
+          :key="c"
+          :class="[
+            'rounded-lg',
+            'w-full',
+            '@tablet-portrait/surface-container:w-40',
+            'h-12',
+            'bg-amber-500',
+          ]"
+        ></div>
       </div>
     </div>
   </div>
