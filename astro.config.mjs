7e7296bc53cd768cc928baab84ad6142265f184e
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2024-01-05 22:01:51 +0800

    use tw plugin again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://css-container-queries-demo.duckwho.codes',
-  integrations: [vue()],
+  integrations: [vue(), tailwind()]
 });
